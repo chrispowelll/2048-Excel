79f8c7985b0bcfb29256d1e333633da3c4d8c8fa
Chris Powell <65933702+chrispowelll@users.noreply.github.com>
2020-08-05 20:22:28 +1000

    Fixed bugs and added game status detection
    - Fixed bug whereby cells could be overwritten by a new value
    - Detection of Game Won/Lost
    - Game Over/Won text appears when the event occurs
    - Detects if a move was made
    - Only adds a new cell if a move was made

--- controls.py
+++ controls.py
@@ -12,6 +12,9 @@ def newGame():
     for i in gameCells:  # game
         wsValues[i] = 0
 
+    # Rest game status
+    wsValues['A6'] = ""
+
     # Reset score
     wsValues['D1'] = 0
 
@@ -27,6 +30,13 @@ def move(direction):
     wb = openpyxl.load_workbook("values.xlsx", read_only=False)
     wsValues = wb.active
     scoreAdded = 0
+
+    # Get current values to check if move is made later
+    originalValues = []
+    for i in ['A', 'B', 'C', 'D']:
+        for j in ['2', '3', '4', '5']:
+            cell = wsValues[str(i) + str(j)].value
+            originalValues.append(cell)
 
     # Moves
     if direction == "up":
@@ -645,13 +655,50 @@ def move(direction):
             scoreAdded += wsValues['B5'].value
             wsValues['A5'] = 0
 
-    # Generate new block in an empty cell
-    emptyCells = ['A2', 'A3', 'A4', 'A5', 'B2', 'B3', 'B4', 'B5', 'C2', 'C3', 'C4', 'C5', 'D2', 'D3', 'D4', 'D5']
-    for i in emptyCells:
-        if wsValues[i].value > 0:
-            emptyCells.remove(i)
-    newBlock = random.choice(emptyCells)
-    wsValues[newBlock] = game.getNewValue()
+    # Get current values to check if move was made
+    updatedValues = []
+    for i in ['A', 'B', 'C', 'D']:
+        for j in ['2', '3', '4', '5']:
+            cell = wsValues[str(i) + str(j)].value
+            updatedValues.append(cell)
+
+    # Check if move was made
+    if updatedValues != originalValues:
+        # Generate list of empty cells
+        emptyCells = []
+        for i in ['A', 'B', 'C', 'D']:
+            for j in ['2', '3', '4', '5']:
+                cell = str(i) + str(j)
+                if wsValues[cell].value == 0:
+                    emptyCells.append(cell)
+
+        # Create a new number of there is an empty cell
+        if len(emptyCells) > 0:
+            newBlock = random.choice(emptyCells)
+            wsValues[newBlock] = game.getNewValue()
+            emptyCells.remove(newBlock)
+
+        # Detect if game is over
+        if len(emptyCells) == 0 and wsValues['A2'].value != wsValues['A3'].value and wsValues['A2'].value != \
+                wsValues['B2'].value and wsValues['B2'].value != wsValues['C2'].value and wsValues['B2'].value != \
+                wsValues['B3'] and wsValues['C2'].value != wsValues['C3'].value and wsValues['C2'].value != \
+                wsValues['D2'].value and wsValues['D2'].value != wsValues['D3'].value and wsValues['A3'].value != \
+                wsValues['A4'].value and wsValues['A3'].value != wsValues['B3'].value and wsValues['B3'].value != \
+                wsValues['B4'].value and wsValues['B3'].value != wsValues['C3'].value and wsValues['C3'].value != \
+                wsValues['C4'].value and wsValues['C3'].value != wsValues['D3'].value and wsValues['D3'].value != \
+                wsValues['D4'].value and wsValues['A4'].value != wsValues['A5'].value and wsValues['A4'].value != \
+                wsValues['B4'].value and wsValues['B4'].value != wsValues['B5'].value and wsValues['B4'].value != \
+                wsValues['C4'].value and wsValues['C4'].value != wsValues['C5'].value and wsValues['C4'].value != \
+                wsValues['D4'].value and wsValues['D5'].value != wsValues['C5'].value and wsValues['C5'].value != \
+                wsValues['B5'].value and wsValues['B5'].value != wsValues['A5'].value:
+            # End game
+            wsValues['A6'] = "GAME OVER!"
+
+        # Detect if game won
+        for i in ['A', 'B', 'C', 'D']:
+            for j in ['2', '3', '4', '5']:
+                if wsValues[str(i) + str(j)].value == 2048:
+                    wsValues['A6'] = "GAME WON!"
 
     # Save, update score and values
     wb.save("values.xlsx")
