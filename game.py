5606cca1cadd1f5f536081a3f87322412d0cbc77
Chris Powell <65933702+chrispowelll@users.noreply.github.com>
2020-08-08 15:23:12 +1000

    Added load game function and timer
    - User can choose to set how long they want to play for
    - Users can choose to continue once the timer expires
    - Users can load saved games

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ import sys
 import random
 import openpyxl
 import xlwings as xw
+import time
 
 # If Windows is used, import WConio2
 if os.name == 'nt':
@@ -17,7 +18,7 @@ if os.name == 'nt':
 def main():
     while True:
         # Original file input
-        wbFilename = input("Enter Excel file path: ")
+        wbFilename = input("Enter your Excel file path: ")
 
         # Check if user wants to quit
         if wbFilename in ('quit', 'Quit', 'QUIT'):
@@ -25,20 +26,78 @@ def main():
 
         # Check if file exists
         elif os.path.exists(wbFilename):
-            # Create workbook
-            design.createValuesWorkbook()
+            # Time limit
+            while True:
+                timeLimitOption = input("Would you like to set a time limit? (Y/N) ").lower()
+                if timeLimitOption in ['yes', 'y']:
+                    timeLimitExists = True
+                    break
+                elif timeLimitOption in ['no', 'n']:
+                    timeLimitExists = False
+                    break
+                else:
+                    print("Please try again. Enter Yes or No")
+            if timeLimitExists:
+                while True:
+                    timeLimit = input("How many minutes would you like to play for? ")
+                    try:
+                        if int(timeLimit) > 0:
+                            break
+                        else:
+                            print("Please enter a number greater than 0")
+                    except:
+                        print("Please enter an integer")
+
+            # Create 2048 workbook
             design.create2048Workbook(wbFilename)
-            controls.newGame()
+
+            # Check if they want to load saved game
+            while True:
+                loadSaveOption = input("Would you like to load a saved game? (Y/N) ").lower()
+                if loadSaveOption in ['yes', 'y']:
+                    break
+                elif loadSaveOption in ['no', 'n']:
+                    # Create workbook
+                    design.createValuesWorkbook()
+                    controls.newGame()
+                    break
+                else:
+                    print("Please try again. Enter Yes or No")
 
             # Open original workbook and 2048 copy
             updateValues()
 
+            # Start game clock
+            if timeLimitExists:
+                startTime = time.time()
+                # Calculate when timer ends
+                endTime = (int(timeLimit)*60) + startTime
+
             # The Game
             while True:
+                # Check if time is up
+                if timeLimitExists:
+                    currentTime = time.time()
+                    if currentTime >= endTime:
+                        print("You have now played for " + timeLimit + " minutes")
+                        # Option to continue playing
+                        while True:
+                            continuePlaying = input("Would you like to continue playing? (Y/N) ").lower()
+                            if continuePlaying in ['yes', 'y']:
+                                timeLimitExists = False
+                                break
+                            elif continuePlaying in ['no', 'n']:
+                                # Quit game
+                                os.startfile(wbFilename)
+                                sys.exit()
+                            else:
+                                print("Please try again. Enter Yes or No")
+
                 if userOS == "Windows":
                     move = WConio2.getkey()
                 else:
                     move = input()
+
                 if move == 'w':
                     controls.move("up")
                 if move == 's':
